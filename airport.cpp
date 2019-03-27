6f26753717cbff7391d6f337ab9a1400f137691d
Beatrix <bealearnscode@gmail.com>
2019-03-27 14:49:40 +0000

    start comments

---
+++ airport.cpp
@@ -0,0 +1,16 @@
+/*
+
+Write a program (airport.cpp) that uses a structure to store the following information for a particular month at the local airport. 
+Total number of planes that landed
+Total number of planes that departed
+Greatest number of planes that landed in a given day that month
+Least number of planes that landed in a given day that month
+This program should have an array of twelve structures to hold travel information for the entire year. The program should prompt the user to enter data for each month. Once all data is entered, the program should calculate and output the average month number of landing planes, the average monthly number of departing planes, the total number of landing and departing planes for the year, and the greatest and least number of planes that landed on any one day (and which month it occurred in).
+
+
+*/
+
+//struct month
+    // int landed
+    // int departed
+    // string
